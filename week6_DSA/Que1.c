4d7231ed5da5050b55dd6824ff505b2de82dc54a
NihalKumar <nihalgmpk@gmail.com>
2023-09-24 15:16:37 +0530

    upload

--- week6_DSA/Que1.c
+++ week6_DSA/Que1.c
@@ -1,102 +1,167 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// Define a structure for a doubly linked list node
-struct Node
+struct node
 {
-    struct Node *prev;
     int data;
-    struct Node *next;
+    struct node *pre;
+    struct node *next;
 };
-
-// Function to create a new node
-struct Node *createNode(int data)
+void linked_display(struct node *current)
 {
-    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->prev = NULL;
-    newNode->next = NULL;
-    return newNode;
-}
-// Function to insert a new node at the beginning of the list
-void insertAtBeginning(struct Node **head, int data)
-{
-    struct Node *newNode = createNode(data);
-    if (*head == NULL)
-    {
-        *head = newNode;
-    }
-    else
-    {
-        newNode->next = *head;
-        (*head)->prev = newNode;
-        *head = newNode;
-    }
-}
-
-// Function to insert a new node at a specific position
-void insertAtPosition(struct Node **head, int data, int position)
-{
-    struct Node *newNode = createNode(data);
-    if (position < 1)
-    {
-        printf("Invalid position!\n");
-        return;
-    }
-    if (position == 1)
-    {
-        insertAtBeginning(head, data);
-        return;
-    }
-
-    struct Node *current = *head;
-    int currentPosition = 1;
-
-    while (currentPosition < position - 1 && current != NULL)
-    {
-        current = current->next;
-        currentPosition++;
-    }
-
-    if (current == NULL)
-    {
-        printf("Invalid position!\n");
-        return;
-    }
-
-    newNode->prev = current;
-    newNode->next = current->next;
-    if (current->next != NULL)
-    {
-        current->next->prev = newNode;
-    }
-    current->next = newNode;
-}
-
-// Function to display the doubly linked list
-void displayList(struct Node *head)
-{
-    struct Node *current = head;
-    printf("\n");
+    printf("NULL <-> ");
     while (current != NULL)
     {
         printf("%d <-> ", current->data);
+        // printf("%p %p %p |||| ",current->pre,current,current->next);
         current = current->next;
     }
-    printf("\n");
+    printf("NULL\n");
+}
+struct node *insertatend(struct node *head)
+{
+    struct node *temp = head;
+
+    struct node *new = (struct node *)malloc(sizeof(struct node));
+    printf("Enter Data for new_Last_ node: ");
+    scanf("%d", &new->data);
+    new->next = NULL;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = new;
+    new->pre = temp;
+    return head;
+}
+struct node *insertatfront(struct node *head){
+    struct node *temp=head;
+    struct node* new=(struct node *)malloc(sizeof(struct node));
+    printf("Enter the Data for New_Front_Node: ");
+    scanf("%d",&new->data);
+    new->pre=NULL;
+    new->next=temp;
+    temp->pre=new;
+    head=new;
+    return head;  
 }
 
+struct node *insertatposition(struct node *head){
+    struct node*current=head;
+    struct node*temp=head;
+    int counter=1,pos;
+    while (current->next!=NULL)
+    {
+        counter++;
+        current=current->next;
+    }
+    printf("Total Nodes in Doubly_Linked_List : %d\n",counter);
+    printf("Enter Position where you want to create a node: ");
+    scanf("%d",&pos);
+    if (pos<1||pos>counter)
+    {
+        printf("!!Invalid Position!!");
+        exit(1);
+    }
+    else if (pos==1)
+    {
+        insertatfront(head);
+    }
+    else{
+        struct node*new=(struct node*)malloc(sizeof(struct node));
+        printf("Enter Data For New_Position_Node: ");
+        scanf("%d",&new->data);
+        for (int i = 0; i < pos-2; i++)
+        {
+            temp=temp->next;
+        }
+        new->next=temp->next;
+        temp->next=new;
+        new->pre=temp;
+        return head;
+    }
+}
+
+struct node * deletionatend(struct node* head){
+    struct node*temp=head;
+    while(temp->next->next!=NULL){
+        temp=temp->next;
+    }
+    struct node*temp2=temp->next;
+    temp2->pre=NULL;
+    printf("\nDisplaying Linked_list \nAfter Last Node %d is De-linked\n",temp2->data);
+    temp->next=NULL;
+    free(temp2);
+    return head;
+}
+struct node *deletionatfirst(struct node*head){
+    struct node*temp=head;
+    head=head->next;
+    temp->next=NULL;
+    free(temp);
+    return head;
+}
+
+
+struct node *deletionatpos(struct node *head){
+    struct node*current=head;
+    struct node*temp=head;
+    int counter=1,pos;
+    while (current->next!=NULL)
+    {
+        counter++;
+        current=current->next;
+    }
+    printf("Total Nodes in Doubly_Linked_List : %d\n",counter);
+    printf("Enter Position where you want to delete a node: ");
+    scanf("%d",&pos);
+    if (pos<1||pos>counter)
+    {
+        printf("!!Invalid Position!!");
+        exit(1);
+    }
+    else if (pos==1)
+    {
+        deletionatfirst(head);
+    }
+    else{
+        for (int i = 0; i < pos-2; i++)
+        {
+            temp=temp->next;
+        }
+        struct node*temp2=temp->next;
+        temp->next=temp2->next;
+        struct node*temp3=temp2->next;
+        temp2->pre=temp;
+        return head;
+    }
+}
 int main()
 {
-    struct Node *head = NULL;
-
-    insertAtBeginning(&head, 10);
-    insertAtPosition(&head, 20, 2);
-    insertAtPosition(&head, 30, 3);
-    insertAtPosition(&head, 40, 4);
-
-    printf("Doubly Linked List after insertions: ");
-    displayList(head);
-
+    struct node *head = (struct node *)malloc(sizeof(struct node));
+    struct node *second = (struct node *)malloc(sizeof(struct node));
+    struct node *third = (struct node *)malloc(sizeof(struct node));
+    head->data = 10;
+    head->pre = NULL;
+    head->next = second;
+    second->data = 20;
+    second->pre = head;
+    second->next = third;
+    third->data = 30;
+    third->pre = second;
+    third->next = NULL;
+    linked_display(head);
+    head = insertatend(head);
+    linked_display(head);
+    head = insertatfront(head);
+    linked_display(head);
+    head=insertatposition(head);
+    linked_display(head);
+    head=deletionatend(head);
+    linked_display(head);
+    printf("After Deletion_at_first, Nodes are :");
+    head=deletionatfirst(head);
+    linked_display(head);
+    head=deletionatpos(head);
+    linked_display(head);
     return 0;
 }
--- week6_DSA/Que1_1.c
+++ week6_DSA/Que1_1.c
@@ -1,29 +1,168 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct node{
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
     int data;
-    struct node*pre;
-    struct node*next;
+    struct node *pre;
+    struct node *next;
 };
-void linked_display(struct node*current){
+void linked_display(struct node *current)
+{
     printf("NULL <-> ");
-    while (current!=NULL)
+    while (current != NULL)
     {
-        printf("%d <-> ",current->data);
+        printf("%d <-> ", current->data);
+        // printf("%p %p %p |||| ",current->pre,current,current->next);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+struct node *insertatend(struct node *head)
+{
+    struct node *temp = head;
+
+    struct node *new = (struct node *)malloc(sizeof(struct node));
+    printf("Enter Data for new_Last_ node: ");
+    scanf("%d", &new->data);
+    new->next = NULL;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = new;
+    new->pre = temp;
+    return head;
+}
+struct node *insertatfront(struct node *head){
+    struct node *temp=head;
+    struct node* new=(struct node *)malloc(sizeof(struct node));
+    printf("Enter the Data for New_Front_Node: ");
+    scanf("%d",&new->data);
+    new->pre=NULL;
+    new->next=temp;
+    temp->pre=new;
+    head=new;
+    return head;  
+}
+
+struct node *insertatposition(struct node *head){
+    struct node*current=head;
+    struct node*temp=head;
+    int counter=1,pos;
+    while (current->next!=NULL)
+    {
+        counter++;
         current=current->next;
     }
-    printf("NULL");
+    printf("Total Nodes in Doubly_Linked_List : %d\n",counter);
+    printf("Enter Position where you want to create a node: ");
+    scanf("%d",&pos);
+    if (pos<1||pos>counter)
+    {
+        printf("!!Invalid Position!!");
+        exit(1);
+    }
+    else if (pos==1)
+    {
+        insertatfront(head);
+    }
+    else{
+        struct node*new=(struct node*)malloc(sizeof(struct node));
+        printf("Enter Data For New_Position_Node: ");
+        scanf("%d",&new->data);
+        for (int i = 0; i < pos-2; i++)
+        {
+            temp=temp->next;
+        }
+        new->next=temp->next;
+        temp->next=new;
+        new->pre=temp;
+        return head;
+    }
 }
-int main(){
-    struct node*head=(struct node*)malloc(sizeof(struct node));
-    struct node*second=(struct node*)malloc(sizeof(struct node));
-    struct node*third=(struct node*)malloc(sizeof(struct node));
-    head->data=10;head->pre=NULL;head->next=second;
-    second->data=20;second->pre=head;second->next=third;
-    third->data=30;third->pre=second;third->next=NULL;
+
+struct node * deletionatend(struct node* head){
+    struct node*temp=head;
+    while(temp->next->next!=NULL){
+        temp=temp->next;
+    }
+    struct node*temp2=temp->next;
+    temp2->pre=NULL;
+    printf("\nDisplaying Linked_list \nAfter Last Node %d is De-linked\n",temp2->data);
+    temp->next=NULL;
+    free(temp2);
+    return head;
+}
+struct node *deletionatfirst(struct node*head){
+    struct node*temp=head;
+    head=head->next;
+    temp->next=NULL;
+    free(temp);
+    return head;
+}
+
+
+struct node *deletionatpos(struct node *head){
+    struct node*current=head;
+    struct node*temp=head;
+    int counter=1,pos;
+    while (current->next!=NULL)
+    {
+        counter++;
+        current=current->next;
+    }
+    printf("Total Nodes in Doubly_Linked_List : %d\n",counter);
+    printf("Enter Position where you want to delete a node: ");
+    scanf("%d",&pos);
+    if (pos<1||pos>counter)
+    {
+        printf("!!Invalid Position!!");
+        exit(1);
+    }
+    else if (pos==1)
+    {
+        deletionatfirst(head);
+    }
+    else{
+        for (int i = 0; i < pos-2; i++)
+        {
+            temp=temp->next;
+        }
+        struct node*temp2=temp->next;
+        temp->next=temp2->next;
+        struct node*temp3=temp2->next;
+        temp2->pre=temp;
+        return head;
+    }
+}
+int main()
+{
+    struct node *head = (struct node *)malloc(sizeof(struct node));
+    struct node *second = (struct node *)malloc(sizeof(struct node));
+    struct node *third = (struct node *)malloc(sizeof(struct node));
+    head->data = 10;
+    head->pre = NULL;
+    head->next = second;
+    second->data = 20;
+    second->pre = head;
+    second->next = third;
+    third->data = 30;
+    third->pre = second;
+    third->next = NULL;
     linked_display(head);
-
-
+    head = insertatend(head);
+    linked_display(head);
+    head = insertatfront(head);
+    linked_display(head);
+    head=insertatposition(head);
+    linked_display(head);
+    head=deletionatend(head);
+    linked_display(head);
+    printf("After Deletion_at_first, Nodes are :");
+    head=deletionatfirst(head);
+    linked_display(head);
+    head=deletionatpos(head);
+    linked_display(head);
 
     return 0;
 }
